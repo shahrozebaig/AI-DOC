33bf9dec1ca13d4b36e1e242dd82f089e43a7060
Shahroze Baig <150318142+shahrozebaig@users.noreply.github.com>
2026-08-20 09:06:02 +0530

    Update llm.py

--- backend/app/core/llm.py
+++ backend/app/core/llm.py
@@ -2,6 +2,6 @@ def get_llm():
     from llama_index.llms.groq import Groq
     from app.config import GROQ_API_KEY
     return Groq(
-        model="llama-3.1-8b-instant",
+        model="openai/gpt-oss-120b",
         api_key=GROQ_API_KEY
     )
